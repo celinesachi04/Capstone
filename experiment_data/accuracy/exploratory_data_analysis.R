fec6f05819f4a5b72ee314ef8ed343aaa2c4c6f5
clogan4 <clogan4@uw.edu>
2026-02-26 14:11:53 -0800

    accuracy eda

--- experiment_data/accuracy/exploratory_data_analysis.R
+++ experiment_data/accuracy/exploratory_data_analysis.R
@@ -1,30 +1,55 @@
 
 library(ggplot2)
+library(dplyr)
+library(patchwork)
 
 response_data <- read.csv("C:/Users/casey/Desktop/Stat 496/Capstone/experiment_data/accuracy/response_data.csv")
-n <- nrow(response_data)
-
-ggplot(response_data, mapping=aes(x=county, y=age, shape=gender, color=what_party)) +
-  geom_point()
-
-# Would want these values to sum to 100
-nrow(response_data %>% filter(vote_democrat=="yes",
-                              vote_republican=="no",
-                              vote_third_party=="no"))
-nrow(response_data %>% filter(vote_democrat=="no",
-                              vote_republican=="yes",
-                              vote_third_party=="no"))
-nrow(response_data %>% filter(vote_democrat=="no",
-                              vote_republican=="no",
-                              vote_third_party=="yes"))
 
 table(response_data %>% select(what_party, vote_democrat))
 table(response_data %>% select(what_party, vote_republican))
 
+sum(ifelse(response_data$energy_initiative == "yes", 1, 0))
+sum(ifelse(response_data$energy_initiative == "no", 1, 0))
+sum(ifelse(response_data$energy_initiative == "unknown", 1, 0))
 
-# Ideally, would want this number to be 1.0
-nrow(response_data %>% filter(vote_democrat=="no",
-                              vote_republican=="yes",
-                              vote_third_party=="no")) / 
-  nrow(response_data %>% filter(what_party=="Republican"))
 
+table(response_data$energy_initiative)
+
+initiative_data <- data.frame("initiative"=c("energy_initiative", "taxes_initiative",
+                                             "carbon_tax_initiative", "insurance_initiative"),
+                              "no"=NA,
+                              "unknown"=NA,
+                              "yes"=NA)
+
+for (initiative in initiative_data$initiative) {
+  summary <- table(response_data[, initiative])
+  initiative_data[initiative_data[, "initiative"]==initiative, -1] <- summary
+}
+
+initiative_data
+
+
+
+
+
+energy <- ggplot(response_data, mapping=aes(x=energy_initiative, fill=age)) + geom_bar() +
+  labs(x="", title="Energy Initiative") + annotate(geom = "text", x = "no", y = 60, label = "62%") + theme_bw()
+taxes <- ggplot(response_data, mapping=aes(x=taxes_initiative, fill=age)) + geom_bar() +
+  labs(x="", title="Taxes Initiative") + theme_bw()
+carbon <- ggplot(response_data, mapping=aes(x=carbon_tax_initiative, fill=age)) + geom_bar() +
+  labs(x="", title="Carbon Tax Initiative") + theme_bw()
+insurance <- ggplot(response_data, mapping=aes(x=insurance_initiative, fill=age)) + geom_bar() +
+  labs(x="", title="Insurance Initiative") + theme_bw()
+
+energy + taxes + carbon + insurance + plot_annotation(title="Predicted initiative votes by age")
+
+energy <- ggplot(response_data, mapping=aes(x=energy_initiative, fill=gender)) + geom_bar() +
+  labs(x="", title="Energy Initiative") + theme_bw()
+taxes <- ggplot(response_data, mapping=aes(x=taxes_initiative, fill=gender)) + geom_bar() +
+  labs(x="", title="Taxes Initiative") + theme_bw()
+carbon <- ggplot(response_data, mapping=aes(x=carbon_tax_initiative, fill=gender)) + geom_bar() +
+  labs(x="", title="Carbon Tax Initiative") + theme_bw()
+insurance <- ggplot(response_data, mapping=aes(x=insurance_initiative, fill=gender)) + geom_bar() +
+  labs(x="", title="Insurance Initiative") + theme_bw()
+
+energy + taxes + carbon + insurance + plot_annotation(title="Predicted initiative votes by county")
